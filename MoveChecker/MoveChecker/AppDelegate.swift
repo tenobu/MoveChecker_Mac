ad7a0ec8d6afb26f1e5e8cd6e34b03859414f9b1
寺内 信夫 <tenobu@gmail.com>
2016-08-08 19:57:22 +0900

    初期化

---
+++ MoveChecker/MoveChecker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MoveChecker
+//
+//  Created by 寺内 信夫 on 2016/08/07.
+//  Copyright © 2016年 寺内 信夫. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ MoveChecker/MoveChecker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MoveChecker
+//
+//  Created by 寺内 信夫 on 2016/08/07.
+//  Copyright © 2016年 寺内 信夫. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
